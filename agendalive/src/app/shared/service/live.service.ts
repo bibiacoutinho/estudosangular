3ef4492b97c8022a4434ea1b733963aadf19459c
Beatriz Coutinho <beatrizmcout@gmail.com>
2021-01-22 09:55:26 -0300

    first commit

---
+++ agendalive/src/app/shared/service/live.service.ts
@@ -0,0 +1,26 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { ResponsePageable } from '../model/responsePageable.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LiveService {
+
+  apiUrl = 'http://localhost:8080/lives';
+
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json'
+    })
+  };
+
+  constructor(
+    private httpCLient: HttpClient
+  ) { }
+
+  public getLivesWithFlag(flag: string): Observable<ResponsePageable> {
+    return this.httpCLient.get<ResponsePageable>(this.apiUrl + '?flag=' + flag);
+  }
+}
